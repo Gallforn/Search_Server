dbfc0c8a46ec81913609091fd77b166262a85966
Maxim <morion.inc@gmail.com>
2022-05-15 16:12:17 +0300

    threads_rework

--- src/InvertedIndex.cpp
+++ src/InvertedIndex.cpp
@@ -19,23 +19,17 @@ void se::InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
     std::recursive_mutex                access_locker;
     size_t                              input_docs_index{0};
     size_t                              input_docs_size{input_docs.size()};
-    std::thread*                        th_1{nullptr};
-    std::thread*                        th_2{nullptr};
-    std::thread*                        th_3{nullptr};
-    auto* th_1_ready =                  new std::atomic<bool>(true);
-    auto* th_2_ready =                  new std::atomic<bool>(true);
-    auto* th_3_ready =                  new std::atomic<bool>(true);
+    std::thread*                        th{nullptr};
+    std::atomic<int>                    thread_counter{0};
 
-    std::function<void(std::string, size_t, std::atomic<bool>*)> text_extract = [&access_locker, this]
-                      (std::string path, size_t index, std::atomic<bool>* is_ready){
-
-        *is_ready = false;
+    std::function<void(std::string, size_t)> text_extract = [&access_locker, &thread_counter, this]
+                      (std::string path, size_t index){
+        ++thread_counter;
 
         std::ifstream file(path, std::ios::app);
 
         if(!file.is_open() || file.fail() || file.bad())
         {
-            *is_ready = true;
             access_locker.lock();
             std::cerr << "Error opening " << path << " file!" << std::endl;
             access_locker.unlock();
@@ -76,77 +70,35 @@ void se::InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
                 word.clear();
             }
         }
-
-        *is_ready = true;
+        --thread_counter;
     };
 
-    do
+    for(; input_docs_index < input_docs_size; ++input_docs_index)
     {
-        if(input_docs_index < input_docs_size && *th_1_ready)
-        {
-            try
-            {
-                th_1 = new std::thread(text_extract, input_docs[input_docs_index], input_docs_index, th_1_ready);
-                ++input_docs_index;
-            }
-            catch(std::exception& exc)
-            {
-                std::cerr << exc.what() << std::endl;
-                --input_docs_index;
-            }
 
+        try
+        {
+            th = new std::thread(text_extract, input_docs[input_docs_index], input_docs_index);
+        }
+        catch(std::exception& exc)
+        {
+            std::cerr << exc.what() << std::endl;
+            --input_docs_index;
         }
 
-        if(input_docs_index < input_docs_size && *th_2_ready)
+        if(th->joinable())
         {
-            try
-            {
-                th_2 = new std::thread(text_extract, input_docs[input_docs_index], input_docs_index, th_2_ready);
-                ++input_docs_index;
-            }
-            catch(std::exception& exc)
-            {
-                std::cerr << exc.what() << std::endl;
-                --input_docs_index;
-            }
+            th->detach();
         }
 
-        if(input_docs_index < input_docs_size && *th_3_ready)
-        {
-            try
-            {
-                th_3 = new std::thread(text_extract, input_docs[input_docs_index], input_docs_index, th_3_ready);
-                ++input_docs_index;
-            }
-            catch(std::exception& exc)
-            {
-                std::cerr << exc.what() << std::endl;
-                --input_docs_index;
-            }
-        }
+    }
 
-        if(th_1 != nullptr && th_1->joinable() && *th_1_ready)
-        {
-            th_1->join();
-        }
-        if(th_2 != nullptr && th_2->joinable() && *th_2_ready)
-        {
-            th_2->join();
-        }
-        if(th_3 != nullptr && th_3->joinable() && *th_3_ready)
-        {
-            th_3->join();
-        }
+    while(thread_counter > 0)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 
-    }while(input_docs_index < input_docs_size || !*th_1_ready || !*th_2_ready || !*th_3_ready);
-
-    delete th_1_ready;
-    delete th_2_ready;
-    delete th_3_ready;
-
-    delete th_1;
-    delete th_2;
-    delete th_3;
+    delete th;
 
     std::cout << "Update document base is over." << std::endl;
 }
